fcad0bd70d0b5db2a71bb98948fb2f2046da0971
Alx Dark <alx.dark@sagebase.org>
2019-05-03 13:33:57 -0700

    Add metrics to tests

--- src/test/java/org/sagebionetworks/bridge/RequestContextTest.java
+++ src/test/java/org/sagebionetworks/bridge/RequestContextTest.java
@@ -9,6 +9,9 @@ import java.util.Set;
 
 import org.junit.Test;
 
+import org.sagebionetworks.bridge.models.Metrics;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.collect.ImmutableSet;
 
 public class RequestContextTest {
@@ -17,7 +20,10 @@ public class RequestContextTest {
     private static final Set<Roles> ROLES = ImmutableSet.of(Roles.DEVELOPER, Roles.WORKER);
 
     @Test
-    public void nullObjectReturnsNullAndEmpty() { 
+    public void builderIsNullSafe() { 
+        // Verify that minimal construction of a context works and returns a valid, if 
+        // empty and useless, request context. It's expected this will be augmented by other
+        // code that executes.
         RequestContext nullContext = new RequestContext.Builder().withRequestId(null).withCallerStudyId(null)
                 .withCallerSubstudies(null).withCallerRoles(null).build();
         
@@ -26,24 +32,41 @@ public class RequestContextTest {
         assertTrue(nullContext.getCallerRoles().isEmpty());
         assertNull(nullContext.getCallerStudyId());
         assertNull(nullContext.getCallerStudyIdentifier());
+        assertNotNull(nullContext.getMetrics());
         
+        ObjectNode node = nullContext.getMetrics().getJson();
+        assertTrue(node.has("request_id"));
+        assertTrue(node.has("version"));
+        assertTrue(node.has("start"));
+    }
+    
+    @Test
+    public void nullInstanceContainsNoValues() {
+        // This is truly devoid of all values and represents no value (in production it's used to 
+        // clear the ThreadLocal variable in a way that prevents making logs of null-pointer checks
+        // in the code).
         assertNull(RequestContext.NULL_INSTANCE.getId());
         assertTrue(RequestContext.NULL_INSTANCE.getCallerSubstudies().isEmpty());
         assertTrue(RequestContext.NULL_INSTANCE.getCallerRoles().isEmpty());
         assertNull(RequestContext.NULL_INSTANCE.getCallerStudyId());
         assertNull(RequestContext.NULL_INSTANCE.getCallerStudyIdentifier());
+        assertNull(RequestContext.NULL_INSTANCE.getMetrics());
     }
 
     @Test
-    public void test() { 
+    public void test() {
+        // An existing metrics object
+        Metrics metrics = new Metrics("requestId");
+        
         RequestContext context = new RequestContext.Builder().withRequestId("requestId")
                 .withCallerStudyId(TestConstants.TEST_STUDY).withCallerSubstudies(SUBSTUDIES)
-                .withCallerRoles(ROLES).build();
+                .withMetrics(metrics).withCallerRoles(ROLES).build();
 
         assertEquals("requestId", context.getId());
         assertEquals(TestConstants.TEST_STUDY_IDENTIFIER, context.getCallerStudyId());
         assertEquals(TestConstants.TEST_STUDY, context.getCallerStudyIdentifier());
         assertEquals(SUBSTUDIES, context.getCallerSubstudies());
         assertEquals(ROLES, context.getCallerRoles());
+        assertEquals(metrics, context.getMetrics());
     }
 }
